14ff443bf6d57568d3d777f931607dcf4a2de99f
Tim Robb <tim@robbmob.com>
2013-01-19 00:29:47 +1300

    Set root to home, redirecting to welcome unless logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+
+	before_filter :check_user_logged_in
+
+	def check_user_logged_in		
+		redirect_to welcome_index_path unless user_signed_in?
+	end
+	
 	def index
 		
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -50,9 +50,9 @@ Aventurine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'home#index'
   
-	resource :home
+	resources :welcome
   resources :crystals
 
   # See how all your routes lay out with "rake routes"
